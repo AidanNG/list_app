1702c7c5dcc74e1b647e0480120acd5af15ca4c7
Aidan Gow <ang180000@utdallas.edu>
2024-08-21 17:22:42 -0500

    Add delete function

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ root = customtkinter.CTk()
 label = customtkinter.CTkLabel(master=root, text="Checklist", font=("Roboto", 24))
 label.pack(pady=12, padx=100)
 
+checkboxes = []
 def new_entry(entry):
     checkbox = customtkinter.CTkCheckBox(master=root, text=entry.get())
     checkbox.pack(pady=12, padx=10)
+    checkboxes.append(checkbox)
+
+def delete_finished_tasks():
+    for check in checkboxes:
+        if check.get() == 1:
+            check.pack_forget()
 
 user_input = customtkinter.StringVar(root)
 
@@ -22,6 +29,9 @@ entry = customtkinter.CTkEntry(master=root, placeholder_text="New Task", textvar
 entry.pack(pady=12, padx=10)
 
 button = customtkinter.CTkButton(master=root, text="Add New Task", command=lambda : new_entry(entry))
+button.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=root, text="Remove Done Tasks", command=lambda : delete_finished_tasks())
 button.pack(pady=12, padx=10)
 
 root.mainloop()
