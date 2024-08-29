a5d585033787849e1c310409daedbfd4ada6c661
Aidan Gow <ang180000@utdallas.edu>
2024-08-29 13:11:55 -0500

    script to initialize csvs to save work

--- init.py
+++ init.py
@@ -1,2 +1,11 @@
 import csv
 
+with open('Active_Tasks.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["task"]
+    writer.writerow(field)
+
+with open('Closed_Tasks.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    field = ["task"]
+    writer.writerow(field)
