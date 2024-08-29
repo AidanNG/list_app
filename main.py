0eac761cc49b06d4b7ede4f3a72b255aa404853e
Aidan Gow <ang180000@utdallas.edu>
2024-08-29 15:10:06 -0500

    fixed csv communication

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import csv
 import pandas as pd
 
 #open csvs into a pandas data frame
-global active_df, closed_df
 active_df = pd.read_csv('Active_Tasks.csv')
 closed_df = pd.read_csv('Closed_Tasks.csv')
 
@@ -29,6 +28,7 @@ def initialize_checkboxes():
         checkboxes.append(checkbox)
 
 def new_entry(entry):
+    global active_df
     checkbox = customtkinter.CTkCheckBox(master=task_frame, text=entry.get())
     checkbox.pack(pady=5, padx=10)
     checkboxes.append(checkbox)
@@ -36,12 +36,13 @@ def new_entry(entry):
     new_index = active_df.index.max() + 1 if not active_df.empty else 0
     active_df.loc[new_index, 'task'] = checkbox.cget("text")
 
-    active_df.to_csv("Active_Tasks.csv", index=True)
+    active_df.to_csv("Active_Tasks.csv", index=False)
 
     # Clear the entry field after adding the task
     entry.delete(0, 'end')
 
 def delete_entry():
+    global active_df, closed_df
     for check in checkboxes[:]:
         if check.get() == 1:
             task_text = check.cget("text")
@@ -54,8 +55,8 @@ def delete_entry():
             closed_df.loc[new_index, 'task'] = task_text
     
     # Save updated DataFrames
-    active_df.to_csv("Active_Tasks.csv", index=True)
-    closed_df.to_csv("Closed_Tasks.csv", index=True)
+    active_df.to_csv("Active_Tasks.csv", index=False)
+    closed_df.to_csv("Closed_Tasks.csv", index=False)
 
 
 user_input = customtkinter.StringVar(root)
@@ -73,7 +74,6 @@ task_frame = customtkinter.CTkFrame(master=root)
 task_frame.pack(pady=12, padx=100)
 initialize_checkboxes()
 
-closed_df.to_csv("Closed_Tasks.csv", index=True)
-active_df.to_csv("Active_Tasks.csv", index=True)
+closed_df.to_csv("Closed_Tasks.csv", index=False)
+active_df.to_csv("Active_Tasks.csv", index=False)
 root.mainloop()
-task_frame.mainloop()
