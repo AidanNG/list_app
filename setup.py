c3bc823bec8e755216e4f5763c392fb3f1d94696
AidanNG <aidanngow@gmail.com>
2024-12-18 15:29:39 -0600

    change main name

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="list_app",
     version="0.1.0",
-    packages=find_packages(where="src"),
     author="Aidan NG",
     author_email="aidanngow@gmail.com",
     description="A simple list application built with Python and customTkinter",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AidanNG/list_app",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     install_requires=[
-        "customtkinter",
+        "customtkinter","numpy"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -25,7 +24,7 @@ setup(
     python_requires=">=3.6",
     entry_points={
         "gui_scripts": [
-            "list_app=list_app.main:main",
+            "list_app=list_app.list_app:main",
         ],
     },
 )
